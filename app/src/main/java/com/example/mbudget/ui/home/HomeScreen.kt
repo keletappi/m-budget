4428d1ae13352bb437a705d8ef0dfdb2c51ab3d7
Mikko Liikanen <mikko.liikanen@gmail.com>
2022-03-22 19:18:54 +0200

    Add content padding to bottom of lists to prevent forced overlapping of bottom items with floating button

--- app/src/main/java/com/example/mbudget/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/mbudget/ui/home/HomeScreen.kt
@@ -5,27 +5,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.text.isDigitsOnly
 import com.example.mbudget.R
 import com.example.mbudget.model.Budget
 import com.example.mbudget.ui.BudgetHeader
@@ -114,7 +104,10 @@ private fun BudgetListing(
     budgets: List<Budget>,
     openBudget: (UUID) -> Unit,
 ) {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(bottom = 96.dp)
+    ) {
         items(budgets) { budget ->
             Surface(
                 modifier = Modifier
