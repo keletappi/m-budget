2ba0a70eb26315fecb7e3391714d0ba82bbc53cc
Mikko Liikanen <mikko.liikanen@gmail.com>
2022-03-22 19:18:54 +0200

    Initial project setup

---
+++ app/src/main/java/com/example/mbudget/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mbudget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("Hello M'Budget")
+                }
+            }
+        }
+    }
+}
